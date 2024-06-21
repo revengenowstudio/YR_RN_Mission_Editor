fdc7e066a6beb82099c7d2972d3ecc65f4691fbb
Zero Fanker <zcyfksn@vip.qq.com>
2024-06-20 22:41:39 -0400

    all version info now linked to Version.h .

--- MissionEditor/Version.h
+++ MissionEditor/Version.h
@@ -3,3 +3,14 @@
 #define PRODUCT_VERSION_MAJOR 2
 #define PRODUCT_VERSION_MINOR 1
 #define PRODUCT_VERSION_REVISION 0
+
+#if !defined(_str)
+	#define _str(x) #x
+#endif
+#define str(x) _str(x)
+
+#define PRODUCT_VERSION_STRING str(PRODUCT_VERSION_MAJOR) \
+	"." str(PRODUCT_VERSION_MINOR) \
+	"." str(PRODUCT_VERSION_REVISION)
+
+
