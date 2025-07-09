dc12465e386864622d02794f432c47ad84507b50
Zero Fanker <zcyfksn@vip.qq.com>
2025-07-08 23:36:09 -0400

    added Secsome and Handama into credits .

--- MissionEditor/Info.cpp
+++ MissionEditor/Info.cpp
@@ -73,6 +73,7 @@ BOOL CInfo::OnInitDialog()
 	CStringA license = R"(FinalAlert 2: Yuri's Revenge Mission Editor
 Copyright (C) 1999-2024 Electronic Arts, Inc.
 Authored by Matthias Wagner, Zero Fanker
+Also credits for Secsome, Handama
 
 This program is free software: you can redistribute it and/or modify
 it under the terms of the GNU General Public License as published by
