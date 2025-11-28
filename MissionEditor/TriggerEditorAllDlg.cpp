495600ce6645e6ceb94a00b5d46c18a340b740ca
Zero Fanker <zcyfksn@vip.qq.com>
2025-11-28 17:02:38 -0500

    clean up

--- MissionEditor/TriggerEditorAllDlg.cpp
+++ MissionEditor/TriggerEditorAllDlg.cpp
@@ -3,51 +3,6 @@
 #include "variables.h"
 #include "functions.h"
 
-enum Controls {
-    SelectedTrigger = 50903,
-    NewTrigger = 50904,
-    CloneTrigger = 50905,
-    DeleteTrigger = 50906,
-    PlaceOnMap = 50907,
-    Type = 50909,
-    Name = 50911,
-    House = 50913,
-    Attachedtrigger = 50916,
-    Disabled = 50917,
-    Easy = 50918,
-    Medium = 50919,
-    Hard = 50920,
-    Eventtype = 50923,
-    NewEvent = 50924,
-    CloneEvent = 50925,
-    DeleteEvent = 50926,
-    EventDescription = 50927,
-    EventList = 50929,
-    EventParameter1Desc = 50930,
-    EventParameter1 = 50931,
-    EventParameter2Desc = 50932,
-    EventParameter2 = 50933,
-    Actionoptions = 50934,
-    Actiontype = 50940,
-    NewAction = 50937,
-    DeleteAction = 50938,
-    CloneAction = 50939,
-    ActionDescription = 50936,
-    ActionList = 50942,
-    ActionParameter1Desc = 50943,
-    ActionParameter1 = 50944,
-    ActionParameter2Desc = 50945,
-    ActionParameter2 = 50946,
-    ActionParameter3Desc = 50947,
-    ActionParameter3 = 50948,
-    ActionParameter4Desc = 50949,
-    ActionParameter4 = 50950,
-    ActionParameter5Desc = 50951,
-    ActionParameter5 = 50952,
-    ActionParameter6Desc = 50953,
-    ActionParameter6 = 50954,
-    SearchReference = 1999
-};
 
 CTriggerEditorAllDlg::CTriggerEditorAllDlg(CWnd* pParent) : 
     CDialog(IDD, pParent)
