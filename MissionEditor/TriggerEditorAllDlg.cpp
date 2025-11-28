7e651f8642078b1f220d33426fd5459313d1a2c1
Zero Fanker <zcyfksn@vip.qq.com>
2025-11-28 17:02:52 -0500

    some progress .

--- MissionEditor/TriggerEditorAllDlg.cpp
+++ MissionEditor/TriggerEditorAllDlg.cpp
@@ -2,7 +2,17 @@
 #include "TriggerEditorAllDlg.h"
 #include "variables.h"
 #include "functions.h"
+#include "inlines.h"
 
+BEGIN_MESSAGE_MAP(CTriggerEditorAllDlg, CDialog)
+    ON_WM_SHOWWINDOW()
+    ON_BN_CLICKED(IDD_TRGR_CLONE_TRIGGER, &CTriggerEditorAllDlg::OnBnClickedTrgrCloneTrigger)
+    ON_BN_CLICKED(IDD_TRGR_DISABLED, OnDisabled)
+    ON_BN_CLICKED(IDD_TRGR_EASY, OnEasy)
+    ON_BN_CLICKED(IDD_TRGR_MEDIUM, OnMedium)
+    ON_BN_CLICKED(IDD_TRGR_HARD, OnHard)
+    ON_CBN_EDITCHANGE(IDD_TRGR_SELECTED_TRIGGER, onEditChangeTriggerType)
+END_MESSAGE_MAP()
 
 CTriggerEditorAllDlg::CTriggerEditorAllDlg(CWnd* pParent) : 
     CDialog(IDD, pParent)
@@ -26,11 +36,15 @@ void CTriggerEditorAllDlg::DoDataExchange(CDataExchange* pDX)
 {
     CDialog::DoDataExchange(pDX);
 
-    DDX_Control(pDX, IDD_TRGR_SELECTED_TRIGGER, m_selectedTrigger);
+    DDX_Control(pDX, IDD_TRGR_SELECTED_TRIGGER, m_triggerType);
     DDX_Control(pDX, IDD_TRGR_NAME, m_triggerName);
     DDX_Control(pDX, IDD_TRGR_HOUSE, m_house);
     DDX_Control(pDX, IDD_TRGR_TYPE, m_persistence);
     DDX_Control(pDX, IDD_TRGR_ATTACHED_TRIGGER, m_nextTrigger);
+    DDX_Control(pDX, IDD_TRGR_MEDIUM, m_medium);
+    DDX_Control(pDX, IDD_TRGR_HARD, m_hard);
+    DDX_Control(pDX, IDD_TRGR_EASY, m_easy);
+    DDX_Control(pDX, IDD_TRGR_DISABLED, m_disabled);
 
     DDX_Control(pDX, IDD_TRGR_EVENT_TYPE, m_eventTypes);
     DDX_Control(pDX, IDD_TRGR_EVENT_LIST, m_eventList);
@@ -47,6 +61,11 @@ void CTriggerEditorAllDlg::DoDataExchange(CDataExchange* pDX)
     DDX_Control(pDX, IDD_TRGR_ACTION_PARAMETER_5, m_eventParam[4]);
     DDX_Control(pDX, IDD_TRGR_ACTION_PARAMETER_6, m_eventParam[5]);
     DDX_Control(pDX, IDD_TRGR_ACTION_DESCRIPTION, m_actionDescription);
+}
+
+BOOL CTriggerEditorAllDlg::PreTranslateMessage(MSG* pMsg)
+{
+    return CDialog::PreTranslateMessage(pMsg);
 }
 
 void CTriggerEditorAllDlg::translateUI()
@@ -98,7 +117,241 @@ void CTriggerEditorAllDlg::UpdateDialog()
 {
     clear();
 
+    CIniFile& ini = Map->GetIniFile();
+    if (m_currentTrigger.IsEmpty()) {
+        return;
+    }
 
+    ListHouses(m_house, FALSE, TRUE, FALSE);
+    ListTriggers(m_nextTrigger);
+    m_nextTrigger.InsertString(0, "<none>");
+
+    auto triggerCopy = ini["Triggers"][m_currentTrigger];
+    if (RepairTrigger(triggerCopy)) {
+        ini.SetString("Triggers", m_currentTrigger, triggerCopy);
+    }
+
+    m_triggerName.SetWindowText(GetParam(triggerCopy, 2));
+    m_house.SetWindowText(TranslateHouse(GetParam(triggerCopy, 0), TRUE));
+    CString attachedTrigger = GetParam(triggerCopy, 1);
+    m_nextTrigger.SetWindowText(attachedTrigger);
+
+    m_disabled.SetCheck((atoi(GetParam(triggerCopy, 3))));
+    m_easy.SetCheck((atoi(GetParam(triggerCopy, 4))));
+    m_medium.SetCheck((atoi(GetParam(triggerCopy, 5))));
+    m_hard.SetCheck((atoi(GetParam(triggerCopy, 6))));
+
+    for (auto i = 0; i < m_nextTrigger.GetCount(); i++) {
+        CString tmp;
+        m_nextTrigger.GetLBText(i, tmp);
+        TruncSpace(tmp);
+        if (tmp == attachedTrigger) {
+            m_nextTrigger.SetCurSel(i);
+        }
+    }
+
+    for (auto const& [type, def] : ini["Tags"]) {
+        CString attTrigg = GetParam(def, 2);
+        if (attTrigg == m_currentTrigger) {
+            m_triggerType.SetWindowText(GetParam(def, 0));
+            break;
+        }
+    }
 }
 
 
+
+void CTriggerEditorAllDlg::onChangeTriggerName()
+{
+    if (m_currentTrigger.IsEmpty()) {
+        return;
+    }
+
+    CIniFile& ini = Map->GetIniFile();
+
+    if (!ini["Triggers"].Exists(m_currentTrigger)) {
+        return;
+    }
+
+    CString newName;
+    m_triggerName.GetWindowText(newName);
+
+    if (newName.GetLength() == 0) {
+        newName = " ";
+    }
+
+    if (newName.Find(",", 0) >= 0) {//newName.SetAt(newName.Find(",",0), 0);
+        newName = newName.Left(newName.Find(",", 0));
+
+        m_triggerName.SetWindowText(newName);
+    }
+
+    ini.SetString("Triggers", m_currentTrigger, SetParam(ini["Triggers"][m_currentTrigger], 2, newName));
+
+    int i;
+    int p = 0;
+    for (auto const& [type, def] : ini["Tags"]) {
+        CString attTrigg = GetParam(def, 2);
+        if (attTrigg == m_currentTrigger) {
+            p++;
+            char c[50];
+            itoa(p, c, 10);
+            CString newVal = newName + " ";
+            newVal += c;
+            ini.SetString("Tags", type, SetParam(ini.GetString("Tags", type), 1, newVal));
+        }
+    }
+    //MessageBox(ini.sections["Triggers"].values[m_currentTrigger],newName);
+    auto triggerCopy = ini["Triggers"][m_currentTrigger];
+    if (RepairTrigger(triggerCopy)) {
+        ini.SetString("Triggers", m_currentTrigger, triggerCopy);
+    }
+    onKillFocusName();
+}
+
+void CTriggerEditorAllDlg::onEditChangeHouse()
+{
+    CIniFile& ini = Map->GetIniFile();
+
+    if (!ini["Triggers"].Exists(m_currentTrigger) || m_currentTrigger.IsEmpty()) {
+        return;
+    }
+
+    CString newHouse;
+    m_house.GetWindowText(newHouse);
+
+    newHouse = TranslateHouse(newHouse);
+
+    newHouse.TrimLeft();
+    TruncSpace(newHouse);
+    if (newHouse.Find(",", 0) >= 0) {
+        newHouse.SetAt(newHouse.Find(",", 0), 0);
+    }
+
+    ini.SetString("Triggers", m_currentTrigger, SetParam(ini["Triggers"][m_currentTrigger], 0, newHouse));
+
+    auto triggerCopy = ini["Triggers"][m_currentTrigger];
+    if (RepairTrigger(triggerCopy)) {
+        ini.SetString("Triggers", m_currentTrigger, triggerCopy);
+    }
+}
+
+void CTriggerEditorAllDlg::onEditChangeNextTrigger()
+{
+    CIniFile& ini = Map->GetIniFile();
+
+    if (!ini["Triggers"].Exists(m_currentTrigger) || m_currentTrigger.IsEmpty()) {
+        return;
+    }
+
+    CString newTrigger;
+    m_nextTrigger.GetWindowText(newTrigger);
+    newTrigger.TrimLeft();
+    TruncSpace(newTrigger);
+
+    if (newTrigger.Find(",", 0) >= 0) {
+        newTrigger.SetAt(newTrigger.Find(",", 0), 0);
+    }
+
+    ini.SetString("Triggers", m_currentTrigger, SetParam(ini["Triggers"][m_currentTrigger], 1, newTrigger));
+}
+
+void CTriggerEditorAllDlg::onKillFocusName()
+{
+    UpdateDialog();
+}
+
+void CTriggerEditorAllDlg::onKillFocus(CWnd* pNewWnd)
+{
+    CDialog::OnKillFocus(pNewWnd);
+    UpdateDialog();
+}
+
+void CTriggerEditorAllDlg::OnShowWindow(BOOL bShow, UINT nStatus)
+{
+    if (!bShow) {
+        if (auto pOldFocus = GetFocus()) {
+            switch (pOldFocus->GetDlgCtrlID())
+            {
+            case IDD_TRGR_NAME: {
+                this->onChangeTriggerName();
+            } break;
+            default:
+                break;
+            }
+        }
+    }
+    CDialog::OnShowWindow(bShow, nStatus);
+}
+
+void CTriggerEditorAllDlg::onEditChangeTriggerType()
+{
+    if (m_currentTrigger.IsEmpty()) {
+        return;
+    }
+
+    CIniFile& ini = Map->GetIniFile();
+
+    if (!ini["Triggers"].Exists(m_currentTrigger)) {
+        return;
+    }
+
+    CString newType;
+    m_triggerType.GetWindowText(newType);
+    TruncSpace(newType);
+
+    int i;
+    for (auto const& [type, def] : ini["Tags"]) {
+        CString attTrigg = GetParam(def, 2);
+        if (attTrigg == m_currentTrigger) {
+            ini.SetString("Tags", type, SetParam(ini["Tags"][type], 0, newType));
+        }
+    }
+
+    auto trigger = ini["Triggers"][m_currentTrigger];
+    if (RepairTrigger(trigger)) {
+        ini.SetString("Triggers", m_currentTrigger, trigger);
+    }
+}
+
+void CTriggerEditorAllDlg::onOptionCheckChanged(const CButton& checkBtn, const int paramPos)
+{
+    if (m_currentTrigger.IsEmpty()) {
+        return;
+    }
+    auto& ini = Map->GetIniFile();
+    auto const& triggersSec = ini["Triggers"];
+
+    if (!triggersSec.Exists(m_currentTrigger)) {
+        return;
+    }
+    auto const checked = checkBtn.GetCheck() != 0;
+    auto const param = checked ? "1" : "0";
+    auto const newVal = SetParam(triggersSec[m_currentTrigger], paramPos, param);
+    ini.SetString("Triggers", m_currentTrigger, newVal);
+}
+
+void CTriggerEditorAllDlg::OnDisabled()
+{
+    onOptionCheckChanged(m_disabled, 3);
+}
+
+void CTriggerEditorAllDlg::OnEasy()
+{
+    onOptionCheckChanged(m_easy, 4);
+}
+
+void CTriggerEditorAllDlg::OnMedium()
+{
+    onOptionCheckChanged(m_medium, 5);
+}
+
+void CTriggerEditorAllDlg::OnHard()
+{
+    onOptionCheckChanged(m_hard, 6);
+}
+
+void CTriggerEditorAllDlg::OnBnClickedTrgrCloneTrigger()
+{
+    // TODO: Add your control notification handler code here
+}
--- MissionEditor/TriggerEditorAllDlg.h
+++ MissionEditor/TriggerEditorAllDlg.h
@@ -14,17 +14,36 @@ public:
 protected:
     virtual BOOL OnInitDialog() override;
     virtual void DoDataExchange(CDataExchange* pDX) override;
+    virtual BOOL PreTranslateMessage(MSG* pMsg) override;
+
+    afx_msg void OnShowWindow(BOOL bShow, UINT nStatus);
+    afx_msg void onChangeTriggerName();
+    afx_msg void onEditChangeHouse();
+    afx_msg void onEditChangeNextTrigger();
+    afx_msg void onKillFocusName();
+    afx_msg void onKillFocus(CWnd* pNewWnd);
+    afx_msg void onEditChangeTriggerType();
+    afx_msg void OnDisabled();
+    afx_msg void OnEasy();
+    afx_msg void OnMedium();
+    afx_msg void OnHard();
 
     void translateUI();
     void clear();
+    void onOptionCheckChanged(const CButton& checkBtn, const int paramPos);
 
+    CString m_currentTrigger;
     // trigger options
     // TODO: add a filter edit
-    CComboBox m_selectedTrigger;
+    CComboBox m_triggerType;
     CEdit m_triggerName;
     CComboBox m_house;
     CComboBox m_persistence;
     CComboBox m_nextTrigger; // sub trigger, sharing my tag
+    CButton	m_medium;
+    CButton	m_hard;
+    CButton	m_easy;
+    CButton	m_disabled;
     // event options
     CComboBox m_eventTypes;
     CListBox m_eventList;
@@ -36,4 +55,7 @@ protected:
     CListBox m_actionList;
     CComboBox m_eventParam[6]; // 0-5
     CEdit m_actionDescription;
+public:
+    DECLARE_MESSAGE_MAP()
+    afx_msg void OnBnClickedTrgrCloneTrigger();
 };
