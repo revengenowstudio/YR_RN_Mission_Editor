revengenowstudio/YR_RN_Mission_Editor

commit 3c19b8caa62490cd2b4df24d2e1edaac93112814

Supporting customized param list for trigger action . (#197)
* close #110, supporting customized param list for trigger action .

* close #112

MissionEditor/TriggerDef.cpp
@@ -29,15 +29,33 @@ static bool IsWaypointFormat(const CString& s)
     return s[0] >= 'A' && s[0] <= 'Z';
 }
 
+void parseParamList(ParamType& type, const CIniFile& ini, const CString& sec)
+{
+    auto const& section = ini.GetSection(sec);
+    auto const valCount = section.GetInteger("FixedValueCount");
+    CString idxStr;
+    type.sequencedValues.reserve(valCount);
+    for (auto idx = 0; idx < valCount; ++idx) {
+        idxStr.Format("%d", idx);
+        type.sequencedValues.emplace_back(section.GetString(idxStr));
+    }
+}
+
 void TriggerDefinitionManager::loadParamTypes(const CIniFile& ini, std::ostream& err)
 {
     for (auto const& [id, def] : ini.GetSection("ParamTypes")) {
         auto const paramTypeId = atoi(id);
         auto const params = SplitParams(def);
+        int listType = 0;
+        bool shouldParseList = true;
+        if (IsNumeric(params[1])) {
+            listType = atoi(params[1]);
+            shouldParseList = false;
+        }
         auto const [it, inserted] = m_paramTypes.try_emplace(paramTypeId,
             ParamType{
                 .paramName = params[0],
-                .listType = atoi(params[1]),
+                .listType = listType,
             });
         if (!inserted) {
             err << "Error: duplicated param type definition index " << id << endl;
@@ -45,6 +63,9 @@ void TriggerDefinitionManager::loadParamTypes(const CIniFile& ini, std::ostream&
         // some params will have 3rd control value
         if (params.size() >= 3) {
             it->second.slotCount = atoi(params[2]);
+        }
+        if (shouldParseList) {
+            parseParamList(it->second, ini, params[1]);
         }
     }
 }

MissionEditor/TriggerDef.h
@@ -18,6 +18,7 @@ struct ParamType
     CString paramName;
     int listType{ 0 }; // PARAMTYPE_NOTHING
     int slotCount{ 0 }; // usually 0. Seems 2 is specially meaningful
+    std::vector<CString> sequencedValues;
 };
 
 struct TriggerEventType

MissionEditor/TriggerEditorAllDlg.cpp
@@ -980,6 +980,19 @@ void CTriggerEditorAllDlg::onSelChangeAction()
     onEditChangeActionType();
 }
 
+void handleParamList(CComboBox& cb, const ParamType& paramType)
+{
+    // legacy way
+    if (paramType.sequencedValues.empty()) {
+        HandleParamList(cb, paramType.listType);
+    }
+    // customized values
+    while (cb.DeleteString(0) != CB_ERR);
+    for (auto const& item : paramType.sequencedValues) {
+        cb.AddString(item);
+    }
+}
+
 void CTriggerEditorAllDlg::onEditChangeActionType()
 {
     if (m_currentTrigger.GetLength() == 0) {
@@ -1037,7 +1050,7 @@ void CTriggerEditorAllDlg::onEditChangeActionType()
     for (auto const& [paramSlot, paramType] : actionDef.paramTypes) {
         auto const& paramDef = paramDefs.at(paramType);
         m_actionParamTexts[slot]->SetWindowText(paramDef.paramName);
-        HandleParamList(m_actionParam[slot], paramDef.listType);
+        handleParamList(m_actionParam[slot], paramDef);
         m_actionParam[slot].SetWindowText(actionN.Params()[paramSlot]);
         m_actionParam[slot].EnableWindow(TRUE);
         validSlots[slot] = true;

MissionEditor/data/FinalAlert2/FAData.ini
@@ -977,7 +977,7 @@ vertic_cornerleft_0=21
 28=速度,0
 29=Voxel碎片ID号,29
 30=路径点,17
-31=木箱类型,18
+31=木箱类型,PT_CrateTypes
 32=语音提示框,19
 33=字符,21
 34=行为,9
@@ -997,6 +997,67 @@ vertic_cornerleft_0=21
 48=数值,0,2
 49=像素动画,23
 50=影片,7
+51=雷达事件,PT_RadarEventTypes
+
+; These value sequence are hardcoded, check dissasmbly at YR 0x7E523C
+; Origianl definitinos are as follows:
+; "Money"
+; "Unit"
+; "HealBase"
+; "Cloak"
+; "Explosion"
+; "Napalm"
+; "Squad"
+; "Darkness"
+; "Reveal"
+; "Armor"
+; "Speed"
+; "Firepower"
+; "ICBM"
+; "Invulnerability"
+; "Veteran"
+; "IonStorm"
+; "Gas"
+; "Tiberium"
+; "Pod"
+[PT_CrateTypes]
+FixedValueCount=19
+0=0 - 钱箱
+1=1 - 随机载具单位
+2=2 - 全体医疗/修复
+3=3 - 永久隐形
+4=4 - C4弹头伤害
+5=5 - 火焰爆炸伤害
+6=6 - 随机步兵小队
+7=7 - 全图黑幕
+8=8 - 显示全地图
+9=9 - 护甲强化
+10=10 - 速度提升
+11=11 - 火力提升
+12=12 - 核弹超武单次
+13=13 - 铁幕无敌（功能残缺）
+14=14 - 经验升星
+15=15 - 离子风暴（无效）
+16=16 - 毒气粒子
+17=17 - 小片矿石
+18=18 - 空降仓（无效）
+
+; Controls for radar events
+; The events, in order, are:
+; (1) Generic Combat Event,
+; (2) Generic Noncombat Event,
+; (3) Dropzone Event,
+; (4) Base Under Attack Event,
+; (5) Harvester Under Attack Event,
+; (6) Enemy Object Sensed Event
+[PT_RadarEventTypes]
+FixedValueCount=6
+0=0 - （红）通用战斗事件
+1=1 - （黄）通用非战斗事件
+2=2 - （黄）空降区事件
+3=3 - （红）基地遭受攻击
+4=4 - （红）矿车遭受攻击
+5=5 - （蓝）探测到敌方单位
 
 [ScriptParams]
 0=占位,0
@@ -1257,7 +1318,7 @@ ExtraValueType=0
 52=更改照明状态（无用）,0,42,0,0,0,0,0,0,0,更改某建筑的局部照明方式。此触发与某一可生产局部照明的建筑相关联。,0,0,52
 53=允许事件触发,-2,14,0,0,0,0,0,0,0,允许被勾选为禁止或触发-54设为禁止的触发运行。,0,1,53
 54=禁止事件触发,-2,14,0,0,0,0,0,0,0,禁止可以正常或被触发-53设为可以运行的触发停止运行。,0,1,54
-55=建立雷达事件,0,43,2,0,0,0,1,0,0,在特定的路径点建立雷达事件。0、3、4红圈　1、2黄圈　5篮圈。,0,1,55
+55=建立雷达事件,0,51,2,0,0,0,1,0,0,在特定的路径点建立雷达事件。0、3、4红圈　1、2黄圈　5篮圈。,0,1,55
 56=开启局部变量...,0,3,0,0,0,0,0,0,0,开启局部变量。局部变量可以是 关闭[0] 或 开启[1]。,0,1,56
 57=关闭局部变量...,0,3,0,0,0,0,0,0,0,关闭局部变量。局部变量可以是 关闭[0] 或 开启[1]。,0,1,57
 58=流星雨在...,0,44,0,0,0,0,1,0,0,在特定的路径点建立显示一个流星雨事件。,0,0,58

commit 75a161c1befdbcdef42b47ed413aa1152a8f634e

Events & Actions following up fixes . (#203)
* address #202, Events & Actions not cleared when saving back to map .
* address #201, first event/action deletion selection loss issue, handle special waypoint slot carefully .
* fixed issue introduced in #197 .
* address #200

MissionEditor/TriggerDatabase.cpp
@@ -43,6 +43,8 @@ void ObjectDatabase<TriggerInstance>::SaveInto(CIniFile& ini, std::ostream& err)
     auto& eventsSec = ini.AddSection(SEC_EVENTS);
     auto& actionsSec = ini.AddSection(SEC_ACTIONS);
     triggerSec.Clear();
+    eventsSec.Clear();
+    actionsSec.Clear();
 
     for (auto const& trigger : items) {
         triggerSec.SetString(trigger.ID(), trigger.Options().Serialize());

MissionEditor/TriggerDef.cpp
@@ -4,8 +4,10 @@
 #include "Helpers.h"
 #include <format>
 #include "TriggerDatabase.h"
+#include "Defines.h"
 
 const ParamType ParamType::Default;
+const ParamType ParamType::WaypointSpecial { "Waypoint", PARAMTYPE_WAYPOINTS };
 
 TriggerDefinitionManager& TriggerDefinitionManager::Instance()
 {
@@ -238,6 +240,26 @@ CString TriggerOptions::Serialize() const
     return ret;
 }
 
+char getEventControlCode(const TriggerEvent& event)
+{
+    auto const paramTypeIdx = event.Type().paramTypes[1];
+    auto const controlCode = TriggerDefinitionManager::Instance().
+        Params().at(paramTypeIdx).slotCount;
+    char buffer[4];
+    _itoa_s(controlCode, buffer, 10);
+    return buffer[0]; // usually this will only be 1 or 2, for TeamType. But mods might expand this value
+}
+
+const TriggerEventType& TriggerEvent::Type() const
+{
+    try {
+        return TriggerDefinitionManager::Instance().Events().at(eventType);
+    }
+    catch (...) {
+        throw std::runtime_error(std::format("event type {} is not registered", eventType));
+    }
+}
+
 CString TriggerEvents::Serialize() const
 {
     constexpr int perEventDataBufferSize = 32;
@@ -251,7 +273,7 @@ CString TriggerEvents::Serialize() const
     for (auto const& event : events) {
         typeStr.Format("%d,", event.eventType);
         ret += typeStr;
-        ret += event.param2.has_value() ? '2' : '0';
+        ret += getEventControlCode(event);
         ret += ',';
         ret += event.param1;
         ret += ',';
@@ -425,7 +447,7 @@ const ParamType& TriggerAction::ParamOperator::lookUpParamType(TriggerAction& ac
         return TriggerDefinitionManager::Instance().Params().at(paramTypeIdx);
 
     }
-    return ParamType::Default;
+    return action.IsUsingWaypointEncoding() ? ParamType::WaypointSpecial : ParamType::Default;
 }
 
 TagInstance::TagInstance(const CString& id, const CString& fullData) :

MissionEditor/TriggerDef.h
@@ -14,6 +14,7 @@ class TriggerActions;
 struct ParamType 
 {
     static const ParamType Default;
+    static const ParamType WaypointSpecial;
 
     CString paramName;
     int listType{ 0 }; // PARAMTYPE_NOTHING
@@ -82,6 +83,8 @@ struct TriggerEvent
     int eventType{ 0 };
     CString param1;
     std::optional<CString> param2;
+
+    const TriggerEventType& Type() const;
 };
 
 class TriggerEvents
@@ -189,8 +192,8 @@ public:
     TriggerAction& Nth(size_t idx) { return actions.at(idx); }
     void DeleteAt(size_t idx) { actions.erase(actions.begin() + idx); }
 
-    TriggerAction& Insert(size_t slot, TriggerAction&& event = {}) {
-        return *actions.emplace(actions.begin() + slot, std::move(event));
+    TriggerAction& Insert(size_t slot, TriggerAction&& action = {}) {
+        return *actions.emplace(actions.begin() + slot, std::move(action));
     }
 
     CString Serialize() const;

MissionEditor/TriggerEditorAllDlg.cpp
@@ -600,7 +600,12 @@ void CTriggerEditorAllDlg::onEditChangeNextTrigger()
 
 void CTriggerEditorAllDlg::onKillFocusName()
 {
+    auto const selected = m_triggerType.GetCurSel();
     UpdateDialog();
+    if (selected != CB_ERR) {
+        m_triggerType.SetCurSel(selected);
+        onSelChangeTrigger();
+    }
 }
 
 void CTriggerEditorAllDlg::onKillFocus(CWnd* pNewWnd)
@@ -801,8 +806,8 @@ void CTriggerEditorAllDlg::onEditChangeEventType()
 
     HandleParamList(m_eventParam1, paramType1.listType);
     HandleParamList(m_eventParam2, paramType2.listType);
-    GetDlgItem(IDC_TRGR_EVENT_P1_TXT)->SetWindowTextA(paramType1.paramName);
-    GetDlgItem(IDC_TRGR_EVENT_P2_TXT)->SetWindowTextA(paramType2.paramName);
+    GetDlgItem(IDC_TRGR_EVENT_P1_TXT)->SetWindowText(paramType1.paramName);
+    GetDlgItem(IDC_TRGR_EVENT_P2_TXT)->SetWindowText(paramType2.paramName);
 
     m_eventParam1.SetWindowText(eventData.param1);
 
@@ -950,7 +955,7 @@ void CTriggerEditorAllDlg::onDeleteEvent()
 
     updateTriggerEvents();
     if (m_eventList.GetCount() > 0) {
-        m_eventList.SetCurSel(eventIdx - 1);
+        m_eventList.SetCurSel(std::max(eventIdx - 1, 0));
     }
 }
 
@@ -984,6 +989,7 @@ void handleParamList(CComboBox& cb, const ParamType& paramType)
     // legacy way
     if (paramType.sequencedValues.empty()) {
         HandleParamList(cb, paramType.listType);
+        return;
     }
     // customized values
     while (cb.DeleteString(0) != CB_ERR);
@@ -1249,6 +1255,6 @@ void CTriggerEditorAllDlg::onDeleteAction()
 
     updateTriggerActions();
     if (m_actionList.GetCount() > 0) {
-        m_actionList.SetCurSel(actionIdx - 1);
+        m_actionList.SetCurSel(std::max(actionIdx - 1, 0));
     }
 }

MissionEditor/data/FinalAlert2/FAData.ini
@@ -947,7 +947,7 @@ vertic_cornerleft_0=21
 ; ParamTypes
 ; Name, ListType, [Code]
 [ParamTypes]
--1=Unused,0,1;not listed in FA2
+-1=未使用,0,1;not listed in FA2
 0=未使用,0
 1=未知,0
 2=所属方,1

MissionEditor/functions.cpp
@@ -255,6 +255,7 @@ void HandleParamList(CComboBox& cb, int type)
 	cb.GetWindowText(oldText);
 
 	switch (type) {
+		default:
 		case PARAMTYPE_NOTHING:
 		{
 			while (cb.DeleteString(0) != CB_ERR);

UnitTest/TriggerDefinition_Test.cpp
@@ -22,6 +22,7 @@ R"(
 3=Stolen by(Unused),0,2,0,1,When money stolen by a thief,0,0,3
 8=Anything,0,0,0,0,When use it alone%1 take effect immediately,0,1,8
 9=Destroyed%1 Units%1 All...,0,2,0,0,When all techno units get destroyed,0,1,9
+23=Team leaves map...,-1,7,0,0,If all members of the team leaves map,0,1,23
 60=Techno Exists,48,46,0,0,So many techno type exists on map,0,1,60,1
 
 [ActionsRA2]
@@ -55,10 +56,12 @@ R"(
     EXPECT_EQ(mgr.Actions().at(130).ra2Allowed, true);
     EXPECT_EQ(mgr.Actions().at(130).yrOnly, true);
 
-    EXPECT_EQ(mgr.Events().size(), 7);
+    EXPECT_EQ(mgr.Events().size(), 8);
     EXPECT_EQ(mgr.Events().at(0).description, "This is an empty event.");
     EXPECT_EQ(mgr.Events().at(2).obsolete, true);
     EXPECT_EQ(mgr.Events().at(8).description, "When use it alone, take effect immediately");
+    EXPECT_EQ(mgr.Events().at(23).paramTypes[0], 7); // attention, ohhhhhh
+    EXPECT_EQ(mgr.Events().at(23).paramTypes[1], -1); // attention, ohhhhhh
     EXPECT_EQ(mgr.Events().at(60).paramTypes[0], 46); // attention, ohhhhhh
     EXPECT_EQ(mgr.Events().at(60).paramTypes[1], 48); // attention, ohhhhhh
 
@@ -159,6 +162,91 @@ TEST(TriggerTest, OptionsSerde)
 
 TEST(TriggerEventTest, EventsSerde)
 {
+
+    std::stringstream testIniContent;
+    testIniContent <<
+        R"(
+[DontSaveAsWP]
+0=5
+1=9
+;2=10
+3=11
+
+[ParamTypes]
+-1=Unused,0,1;not listed in FA2
+0=Nothing,0
+1=Unknown,0
+2=Affiliated Side,1
+3=Local Variable,20
+4=Time,0
+5=Score,0
+6=Value,0
+7=Squad Type,2
+8=Building,6
+9=Aircraft,5
+10=Infantry,4
+11=Unit,3
+12=Movie,7
+13=Text,8
+14=Trigger,9
+15=Allowed,10
+16=Sound,11
+17=Music,12
+18=Voice,13
+19=Step,0
+20=Super Weapon,14
+21=Left,0
+22=Top,0
+23=Width,0
+24=Height,0
+25=Animation,15
+26=Particle,16
+27=Duration,0
+28=Speed,0
+29=Voxel Fragment ID,29
+30=Way Point,17
+31=Wooden Crate Type,18
+32=Voice Prompt Box,19
+33=Character,21
+34=Action,9
+35=Global Variable,27
+36=Specific Weapon,14
+37=Activated,10
+38=Trigger Tag,22
+39=Technology Type,0
+40=Source,0
+41=Weapon,24
+42=Glow Behavior,25
+43=Event,9
+44=Rain,26
+45=Float Value,0
+46=Technology Type,29
+47=Building,28
+48=Value,0,2
+49=Pixel Animation,23
+50=Film,7
+
+[EventsRA2]
+0=-No Event-,0,0,0,0,This is an empty event.,0,1,0
+1=Enter...,0,2,0,0,Ground unit enters,0,1,1
+2=Discover starts(Unused),0,0,0,1,When a spy enters,0,0,2
+3=Stolen by(Unused),0,2,0,1,When money stolen by a thief,0,0,3
+8=Anything,0,0,0,0,When use it alone%1 take effect immediately,0,1,8
+9=Destroyed%1 Units%1 All...,0,2,0,0,When all techno units get destroyed,0,1,9
+13=Time Elapse...,0,6,0,0,Time elapse in seconds and reset,0,1,13
+23=Team leaves map...,-1,7,0,0,If all members of the team leaves map,0,1,23
+36=Local variable on...,0,3,0,0,If local variable is true,0,1,36
+37=Local variable off...,0,3,0,0,If local variable is false,0,1,37
+60=Techno Exists,48,46,0,0,So many techno type exists on map,0,1,60,1
+61=Techno Not Exists,48,46,0,0,Techno type no longer exists on map,0,1,61,1
+)";
+
+    CIniFile ini;
+    EXPECT_EQ(ini.InsertStream(testIniContent), 0);
+
+    auto& mgr = TriggerDefinitionManager::Instance();
+    mgr.LoadFrom(ini, std::cerr);
+
     // single event
     {
         const CString data = "1,13,0,10";
@@ -213,6 +301,17 @@ TEST(TriggerEventTest, EventsSerde)
         EXPECT_EQ(events.Nth(2).eventType, 37);
         EXPECT_EQ(events.Nth(2).param1, "9");
         EXPECT_EQ(events.Nth(2).param2, std::nullopt);
+
+        EXPECT_EQ(events.Serialize(), data);
+    }
+    {
+        const CString data = "1,23,1,01000023";
+        TriggerEvents events(data);
+
+        EXPECT_EQ(events.Size(), 1);
+        EXPECT_EQ(events.Nth(0).eventType, 23);
+        EXPECT_EQ(events.Nth(0).param1, "01000023");
+        EXPECT_EQ(events.Nth(0).param2, std::nullopt);
 
         EXPECT_EQ(events.Serialize(), data);
     }