53c7f55a40c1dc40a775844cbc37b2ca0b424219
Zero Fanker <zcyfksn@vip.qq.com>
2025-12-08 16:29:40 -0500

    AITrigger enable.

--- MissionEditor/AiTriggerTypesEnable.cpp
+++ MissionEditor/AiTriggerTypesEnable.cpp
@@ -60,6 +60,31 @@ void CAiTriggerTypesEnable::DoDataExchange(CDataExchange* pDX)
 	//}}AFX_DATA_MAP
 }
 
+BOOL CAiTriggerTypesEnable::PreTranslateMessage(MSG* pMsg)
+{
+	int ret = -1;
+	if (pMsg->message == WM_KEYDOWN) {
+		ret = onMessageKeyDown(pMsg);
+	}
+
+	return ret < 0 ? this->CDialog::PreTranslateMessage(pMsg) : ret;
+}
+
+BOOL CAiTriggerTypesEnable::onMessageKeyDown(MSG* pMsg)
+{
+	switch (pMsg->wParam) {
+	default:
+		return -1;
+		case VK_RETURN:
+		{
+			switch (::GetDlgCtrlID(pMsg->hwnd)) {
+			default:
+				break;// never exist window (default -1) even nothing did
+			}
+		}
+	}
+	return TRUE;
+}
 
 BEGIN_MESSAGE_MAP(CAiTriggerTypesEnable, CDialog)
 	//{{AFX_MSG_MAP(CAiTriggerTypesEnable)
