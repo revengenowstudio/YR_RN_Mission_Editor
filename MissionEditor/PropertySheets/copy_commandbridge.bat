b27863d9b6ddea26e5308d1a08d65be845c7814f
Zero Fanker <zcyfksn@vip.qq.com>
2026-06-07 12:19:50 -0400

    added post helper script step .

---
+++ MissionEditor/PropertySheets/copy_commandbridge.bat
@@ -0,0 +1,22 @@
+@echo off
+setlocal enabledelayedexpansion
+
+set "CONFIG=%~1"
+set "PLATFORM=%~2"
+set "OUTDIR=%~3"
+
+if "%CONFIG%"=="" exit /b 0
+if "%PLATFORM%"=="" set "PLATFORM=x64"
+
+set "BRIDGE_CONFIG=Release"
+echo %CONFIG% | findstr /i "debug" >nul && set "BRIDGE_CONFIG=Debug"
+
+set "PROJECT_ROOT=%~dp0..\.."
+set "SRC=%PROJECT_ROOT%\CommandBridge\%PLATFORM%\%BRIDGE_CONFIG%\CommandBridge.dll"
+set "PDB=%PROJECT_ROOT%\CommandBridge\%PLATFORM%\%BRIDGE_CONFIG%\CommandBridge.pdb"
+
+if not exist "%SRC%" exit /b 0
+if "%OUTDIR%"=="" set "OUTDIR=%PROJECT_ROOT%\dist\FinalRevenge"
+
+copy /Y "%SRC%" "%OUTDIR%\" >nul 2>&1
+if exist "%PDB%" copy /Y "%PDB%" "%OUTDIR%\" >nul 2>&1
