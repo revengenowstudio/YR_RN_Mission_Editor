92eaced6c7cf85ce726a7cf2d9a7eade49feccb0
Zero Fanker <zcyfksn@vip.qq.com>
2026-06-07 14:27:13 -0400

    Enhance: also copy header in helper script. Added manual helper script as well .

--- MissionEditor/PropertySheets/copy_commandbridge.bat
+++ MissionEditor/PropertySheets/copy_commandbridge.bat
@@ -14,9 +14,12 @@ echo %CONFIG% | findstr /i "debug" >nul && set "BRIDGE_CONFIG=Debug"
 set "PROJECT_ROOT=%~dp0..\.."
 set "SRC=%PROJECT_ROOT%\CommandBridge\%PLATFORM%\%BRIDGE_CONFIG%\CommandBridge.dll"
 set "PDB=%PROJECT_ROOT%\CommandBridge\%PLATFORM%\%BRIDGE_CONFIG%\CommandBridge.pdb"
+set "HEADER_SRC=%PROJECT_ROOT%\CommandBridge\CommandBridge.h"
+set "HEADER_DST=%PROJECT_ROOT%\3rdParty\CommandBridge\CommandBridge.h"
 
 if not exist "%SRC%" exit /b 0
 if "%OUTDIR%"=="" set "OUTDIR=%PROJECT_ROOT%\dist\FinalRevenge"
 
 copy /Y "%SRC%" "%OUTDIR%\" >nul 2>&1
 if exist "%PDB%" copy /Y "%PDB%" "%OUTDIR%\" >nul 2>&1
+if exist "%HEADER_SRC%" copy /Y "%HEADER_SRC%" "%HEADER_DST%" >nul 2>&1
---
+++ copy_bridge_debug.bat
@@ -0,0 +1,2 @@
+@echo off
+call "%~dp0MissionEditor\PropertySheets\copy_commandbridge.bat" Debug x64 "%~dp0dist\FinalRevenge\"
